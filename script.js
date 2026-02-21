4ac09d5bb391f2e53534a164d2f475d34b617b9a
minhajdoc7x-oss <minhajdoc.7x@gmail.com>
2026-02-21 13:43:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login() {
     let user = document.getElementById("username").value;
     let pass = document.getElementById("password").value;
 
-    if(user === "admin" && pass === "1234") {
+    if(user === "minhaj" && pass === "1234") {
         document.getElementById("loginPage").style.display = "none";
         document.getElementById("mainPage").style.display = "block";
     } else {
